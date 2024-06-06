c189620b16556fe9440b142a38c1e55bcfb5afe1
Albert T <albert.terrazas@gmail.com>
2024-06-06 13:30:26 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from "./componets/header/header"
-import PortfolioCon from "./componets/PortFile"
+import PortfolioCon from "./PortFile"
 import Footer from "./componets/footer/footer"
 
 function App() {
