ff481e6b06c42309b913b445b4a6d9df0ab71eb2
Albert T <albert.terrazas@gmail.com>
2024-06-07 14:33:06 -0500

    Update projects.jsx

--- src/componets/projects.jsx
+++ src/componets/projects.jsx
@@ -2,21 +2,21 @@ export const projects = [
     {
         title: "Tech Blog 1",
         description: "This application was created as the start of a full stack social network application using a MongoDB database, Express.js routing, and the Mongoose ODM.",
-        image: "../images/techblog.png",
+        image: "/techblog.png",
         link: "https://git.heroku.com/whispering-cove-34404.git",
         gitHub: "https://github.com/Fitbert/Techblog.git",
     },
     {
         title: "Code Quiz",
         description: "Bootcamp code quiz to test your knowledge with a timer and high score board.",
-        image: "../images/codequiz.jpg",
+        image: "/codequiz.jpg",
         link: "https://fitbert.github.io/Bootcamp-Coder-Quiz/index.html",
         gitHub: "https://github.com/Fitbert/Bootcamp-Coder-Quiz.git",
     },
     {
         title: "My Weather App",
         description: "The Weather Dashboard shows a 5 days forecast with additional information such as uv index and wind and humidity. This dash will show previous searches to recall for cities and store locally." ,
-        image: "../images/weatherapp.png",
+        image: "/weatherapp.png",
         link: "https://fitbert.github.io/my-weather-app/",
         gitHub: "https://github.com/Fitbert/my-weather-app.git",
     }
